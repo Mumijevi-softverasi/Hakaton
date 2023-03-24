8305788bf69a5284965af6ec52a13b47a46922dd
Nemanja <nemanjadj102@gmail.com>
2023-03-24 22:37:07 +0100

    initial commit

---
+++ src/test/java/org/unibl/etf/hakaton/HakatonApplicationTests.java
@@ -0,0 +1,13 @@
+package org.unibl.etf.hakaton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HakatonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
